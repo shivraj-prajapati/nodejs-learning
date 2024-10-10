8127e1876bf6f0c324a5748b0074165955d37070
Shivraj Prajapati <rocky1989sp@gmail.com>
2024-10-10 21:53:24 +0530

    5day program commited

---
+++ day5/app.js
@@ -0,0 +1,20 @@
+// 1. console.log()
+// Prints a message to stdout.
+console.log("Hello Coders.!");
+console.log("Value of x is:", 10);
+
+// 2. console.error()
+// Prints an error message to stderr.
+console.error("Hey guys i am an error message");
+
+// 3. console.warn()
+// Prints a warning message to stderr.
+console.warn("This is a warning message");
+
+// 4. console.info()
+// Prints an informational message to stdout (same as console.log()).
+console.info("This is an informational message");
+
+// 5. console.debug()
+// Prints a debug message to stdout (same as console.log()).
+console.debug("This is a debug message");
---
+++ day5/notes.md
@@ -0,0 +1,26 @@
+# Day 5: Console Common methods in JavaScript
+
+1. **console.log()**
+   ```javascript
+   console.log("Hello Coders.!");
+   ```
+
+2. **console.error()**
+   ```javascript
+   console.error("This is an error message");
+   ```
+
+3. **console.warn()**
+   ```javascript
+   console.warn("This is a warning");
+   ```
+
+4. **console.info()**
+   ```javascript
+   console.info("This is an info message");
+   ```
+
+5. **console.debug()**
+   ```javascript
+   console.debug("This is a debug message");
+   ```
