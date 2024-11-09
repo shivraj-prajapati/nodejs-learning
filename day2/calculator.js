5604e80cd604e8e38a0f8cef0a389a692ecea597
Shivraj Prajapati <148106824+shivraj-prajapati@users.noreply.github.com>
2024-11-09 08:34:44 +0530

    Update calculator.js

--- day2/calculator.js
+++ day2/calculator.js
@@ -1,7 +1,7 @@
 // Represents the current module in Node.js, allowing exports of its properties and methods.
 module.exports = {
-    add : (a, b) => a + b,
-    sub : (a, b) => a - b,
-    mul : (a, b) => a * b,
-    div : (a, b) => a / b
+    add : (num1, num2) => num1 + num2,
+    sub : (num1, num2) => num1 - num2,
+    mul : (num1, num2) => num1 * num2,
+    div : (num1, num2) => num1 / num2
 };
